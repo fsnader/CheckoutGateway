730eb8c281e3df2a02a899d2f919eb2d5c69f3ea
Felipe Nader <fsnader@gmail.com>
2023-05-27 17:42:44 -0300

    Initial api structure

--- src/CheckoutGateway/CheckoutGateway/Program.cs
+++ src/CheckoutGateway/CheckoutGateway/Program.cs
@@ -1,6 +1,42 @@
+using System.Text.Json.Serialization;
+using CheckoutGateway.Api.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<GlobalExceptionsMiddleware>();
+
+builder.Services.AddLogging(config =>
+{
+    config.AddConsole();
+    config.AddDebug();
+});
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseMiddleware<GlobalExceptionsMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
