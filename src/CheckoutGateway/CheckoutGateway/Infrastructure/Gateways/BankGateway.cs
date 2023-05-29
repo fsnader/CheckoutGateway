b0d8abd8351479d7b3446ca7b888918bdaa016b9
Felipe Nader <fsnader@gmail.com>
2023-05-29 15:39:49 -0300

    Improve mock

--- src/CheckoutGateway/CheckoutGateway/Infrastructure/Gateways/BankGateway.cs
+++ src/CheckoutGateway/CheckoutGateway/Infrastructure/Gateways/BankGateway.cs
@@ -5,12 +5,35 @@ namespace CheckoutGateway.Infrastructure.Gateways;
 
 public class BankGateway : IBankGateway
 {
+    private readonly Random _random;
+    public BankGateway()
+    {
+        _random = new Random();
+    }
     public async Task<PaymentGatewayResponse> ProcessPaymentAsync(Payment payment, CancellationToken cancellationToken)
     {
-        return new PaymentGatewayResponse
+        await Task.Delay(200, cancellationToken);
+
+        return _random.Next(1, 3) switch
         {
-            Id = Guid.NewGuid(),
-            Success = true
+            1 => new PaymentGatewayResponse
+            {
+                Id = Guid.NewGuid(),
+                Success = true
+            },
+            2 => new PaymentGatewayResponse
+            {
+                Id = Guid.NewGuid(),
+                Success = false,
+                Error = "Connection error"
+            },
+            3 => new PaymentGatewayResponse
+            {
+                Id = Guid.NewGuid(),
+                Success = false,
+                Error = "Transaction denied. Customer doesn't have funds."
+            },
+            _ => throw new ArgumentOutOfRangeException()
         };
     }
 }
